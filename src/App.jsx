3dd2703fae2c745542026f654d7f6b3ba94afd73
GabrielSerafim12 <gserafim794@gmail.com>
2024-12-10 18:39:52 -0300

    enviando projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import BuscaDeFilmes from './components/Busca_filmes'
+
+function App() {
+
+  return (
+    <>
+      <BuscaDeFilmes />
+    </>
+  )
+}
+
+export default App
